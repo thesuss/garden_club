fd69ac7955858d3094bf64948c08f3cdddb3f4ad
Amber <amber@amberwilkie.com>
2016-09-12 17:06:49 +0200

    Remove bcrypt bits for now

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,5 @@ class User < ApplicationRecord
                     uniqueness: true,
                     format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
   validates :password, presence: true, confirmation: true
-  # , uniqueness: { case_sensitive: false } --> want to get this working on email
 
 end
