c2c44cce46709b1aeda83a7db036fc97c96ca5b7
Amber <amber@amberwilkie.com>
2016-09-12 15:34:52 +0200

    Refactor error messages for user registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,12 @@ class UsersController < ApplicationController
   def create
     @user = User.new(email: user_params[:email],
                      name: user_params[:name],
-                     password: user_params[:password]) # With all the validations beforehand.
+                     password: user_params[:password])
     if @user.save
       flash[:success] = "Successfully registered"
       redirect_to registration_index_path
     else
-      flash[:error] = "You could not be registered"
+      flash[:error] = @user.errors.full_messages.first
       redirect_to registration_index_path
     end
   end
