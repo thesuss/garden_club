6fd715cd0d63ee0399241a4efa824a6f0be990a5
Amber <amber@amberwilkie.com>
2016-09-12 16:04:57 +0200

    Pass params from user reg form to user model through user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 class UsersController < ApplicationController
   def create
     @user = User.new(email: user_params[:email],
+                     email_confirmation: user_params[:email_confirmation],
                      name: user_params[:name],
-                     password: user_params[:password])
+                     password: user_params[:password],
+                     password_confirmation: user_params[:password_confirmation])
     if @user.save
       flash[:success] = "Successfully registered"
       redirect_to registration_index_path
