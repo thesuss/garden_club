01ed63b787882d0b8de18ff02f5235029bac7e7e
Amber <amber@amberwilkie.com>
2016-09-12 14:59:55 +0200

    Add password and email confirmations in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   validates :name, presence: true
-  validates :email, presence: true
-  validates :password, presence: true
+  validates :email, presence: true, confirmation: true
+  validates :password, presence: true, confirmation: true
   validates_uniqueness_of :email
+
 end
