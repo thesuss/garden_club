8b78ac68d282fb4c4d8be4119cedf4a45a46a1b9
Amber <amber@amberwilkie.com>
2016-09-12 16:37:38 +0200

    Spec test for password length validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,6 +15,7 @@ RSpec.describe User, type: :model do
     it { is_expected.to validate_uniqueness_of :email }
     it { is_expected.to validate_confirmation_of :email }
     it { is_expected.to validate_confirmation_of :password }
+    it { is_expected.to validate_length_of :password }
   end
 
   describe 'Factory' do
