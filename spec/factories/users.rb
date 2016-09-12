b8bf60ace162f9edfcfe74619327ad7f344cbb1b
Amber <amber@amberwilkie.com>
2016-09-12 14:50:19 +0200

    Write tests for uniqueness and factory of user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
     name "MyString"
-    email "MyString"
-    password "MyString"
+    email "MyEmail"
+    password "MyPassword"
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,6 +12,13 @@ RSpec.describe User, type: :model do
     it { is_expected.to validate_presence_of :name }
     it { is_expected.to validate_presence_of :email }
     it { is_expected.to validate_presence_of :password }
+    it { is_expected.to validate_uniqueness_of :email }
+  end
+
+  describe 'Factory' do
+    it 'should have valid Factory' do
+      expect(FactoryGirl.create(:user)).to be_valid
+    end
   end
 
 end
