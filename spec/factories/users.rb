45a13bc09b5055282d018267dd528023b0a93236
thesuss <lullisuss@gmail.com>
2016-09-13 15:05:58 +0200

    removed email confirmation from factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,6 @@ FactoryGirl.define do
   factory :user do
     name "My Name"
     email "My@Email.com"
-    email_confirmation "My@Email.com"
     password "MyPassword"
     password_confirmation "MyPassword"
   end
