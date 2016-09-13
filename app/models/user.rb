4759bf59c48ff47e0e1baaaa8911279ece071a50
Amber <amber@amberwilkie.com>
2016-09-13 11:41:41 +0200

    Remove User model, now we'll try Devise

--- app/models/user.rb
+++
@@ -1,9 +0,0 @@
-class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true,
-                    confirmation: true,
-                    uniqueness: true,
-                    format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
-  validates :password, presence: true, confirmation: true
-
-end
--- spec/factories/users.rb
+++
@@ -1,9 +0,0 @@
-FactoryGirl.define do
-  factory :user do
-    name "My Name"
-    email "My@Email.com"
-    email_confirmation "My@Email.com"
-    password "MyPassword"
-    password_confirmation "MyPassword"
-  end
-end
