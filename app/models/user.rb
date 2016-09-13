9a06e2c612f52afb36d689c05b56187dd29741f1
thesuss <lullisuss@gmail.com>
2016-09-13 12:13:44 +0200

    name cant be blank

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   # validates_presence_of :name
+  validates :name,
+  :presence => true,
+  :uniqueness => {
+    :case_sensitive => false
+  }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160913095735) do
+ActiveRecord::Schema.define(version: 20160913100759) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 20160913095735) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "name",                                null: false
+    t.string   "name",                   default: "", null: false
     t.string   "email_confirmation"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
