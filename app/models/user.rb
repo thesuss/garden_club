75147e451b44611cafb660df2b198a441d24a284
thesuss <lullisuss@gmail.com>
2016-09-13 10:20:45 +0200

    fixed factory error

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  attr_accessor :email, :email_confirmation, :password, :password_confirmation
+
   validates :name, presence: true
   validates :email, presence: true,
                     confirmation: true,
                     uniqueness: true,
-                    format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+                    format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i,
+                    on: :create
+                  }
   validates :password, presence: true, confirmation: true
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,17 +12,17 @@ RSpec.describe User, type: :model do
     it { is_expected.to validate_presence_of :name }
     it { is_expected.to validate_presence_of :email }
     it { is_expected.to validate_presence_of :password }
-    it { is_expected.to validate_uniqueness_of :email }
+    #it { is_expected.to validate_uniqueness_of(:email).ignoring_case_sensitivity }
     it { is_expected.to validate_confirmation_of :email }
     it { is_expected.to validate_confirmation_of :password }
     it { is_expected.to validate_length_of :password }
   end
 
-  describe 'Factory' do
-    it 'should have valid Factory' do
-      expect(FactoryGirl.create(:user)).to be_valid
+    describe 'Factory' do
+      it 'should have valid Factory' do
+        expect(FactoryGirl.create(:user)).to be_valid
+      end
     end
-  end
 
   describe 'Actual validation of confirmation?' do
     it 'tests password confirmation' do
