c95822135b401b501c0436df21e649ccda408a0f
thesuss <lullisuss@gmail.com>
2016-09-14 13:25:53 +0200

    Publish article button can be pressed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,15 +5,21 @@ class ArticlesController < ApplicationController
     render 'users/articles/new'
   end
 
+  def show
+    @article = Article.find(params[:id])
+  end
 
   def create
     @user = User.find(params[:user_id])
     @article = Article.new(article_params)
     @article.save
-    redirect_to article_path(@user)
+    render 'users/articles/show'
+    #redirect_to user_articles_path(@user)
   end
 
+  def index
 
+  end
   private
     def article_params
        params.require(:article).permit(:title, :body)
