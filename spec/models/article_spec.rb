94bc9ba749f3d07e30ff1216a8faf17e0507641a
thesuss <lullisuss@gmail.com>
2016-09-14 11:00:08 +0200

    Validates presence of article title and body

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -8,4 +8,10 @@ RSpec.describe Article, type: :model do
     it { is_expected.to have_db_column :body }
     it { is_expected.to have_db_column :user_id }
   end
+
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of :title }
+    it { is_expected.to validate_presence_of :body }
+  end
+
 end
