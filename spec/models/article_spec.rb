820c012a6d44e27722fbe38a6714401b128f02d6
Amber <amber@amberwilkie.com>
2016-09-14 10:48:07 +0200

    Add regression test for Article model

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'DB table' do
+    it { is_expected.to have_db_column :id }
+    it { is_expected.to have_db_column :title }
+    it { is_expected.to have_db_column :body }
+    it { is_expected.to have_db_column :user_id }
+  end
 end
