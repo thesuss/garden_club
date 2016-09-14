d577c6eaa1f7e1132e39321f6b99ef3e784ad202
thesuss <lullisuss@gmail.com>
2016-09-14 17:29:38 +0200

    refactored article controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,10 @@
 class ArticlesController < ApplicationController
   def new
-    #@user = User.find(params[:user_id])
     @article = Article.new(user_id: current_user.id)
-    render 'users/articles/new'
   end
 
   def show
     @article = Article.find(params[:id])
-    render 'users/articles/show'
-
   end
 
   def create
@@ -19,10 +15,9 @@ class ArticlesController < ApplicationController
 
   def index
     @article = Article.all
-    render 'users/articles/index'
   end
   private
-    def article_params
-       params.require(:article).permit(:title, :body, :user_id)
-    end
+  def article_params
+     params.require(:article).permit(:title, :body, :user_id)
+  end
 end
