a75a33bbe9138c9dc663068d1a38b43b382914aa
thesuss <lullisuss@gmail.com>
2016-09-15 09:23:49 +0200

    refactored errorhandling in articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,7 @@ class ArticlesController < ApplicationController
       @article = Article.new(user_id: current_user.id)
     else
       redirect_to new_user_session_path
-      flash[:notice] = "You are not logged in - you need to be logged in to see this page"
+      flash[:notice] = error_message
     end
   end
 
@@ -19,7 +19,7 @@ class ArticlesController < ApplicationController
       redirect_to user_article_path(@user.id, @article.id)
     else
       redirect_to new_user_session_path
-      flash[:notice] = "You are not logged in - you need to be logged in to see this page"
+      flash[:notice] = error_message
     end
   end
 
@@ -28,11 +28,15 @@ class ArticlesController < ApplicationController
       @article = Article.all
     else
       redirect_to new_user_session_path
-      flash[:notice] = "You are not logged in - you need to be logged in to see this page"
+      flash[:notice] = error_message
     end
   end
   private
   def article_params
      params.require(:article).permit(:title, :body, :user_id)
+  end
+
+  def error_message
+    "You are not logged in - you need to be logged in to see the page you were trying to reach"
   end
 end
