6b469ba56ca96c15bb465cf6f0ea08d8368cd97c
Amber Wilkie <amber@amberwilkie.com>
2016-09-17 21:22:06 +0200

    Make user profile links actually go to a real website (#22)
    * Append http:// to garden websites if they lack it, so they actually link
    
    * Strip garden websites of http://
    
    * Reverse order of articles on front page
    
    * Limit articles on home page to only most recent ten

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,4 +1,16 @@
 class ProfileController < ApplicationController
   def index
+    @user = User.find_by_id(params[:user_id])
+    @user_garden_website = url_with_protocol(@user.garden_website)
+    @display_garden_website = url_no_protocol(@user.garden_website)
+  end
+
+  private
+  def url_with_protocol(url)
+    /^http/i.match(url) ? url : "http://#{url}"
+  end
+
+  def url_no_protocol(url)
+    /^http/i.match(url) ? url.sub(/^https?\:\/\//, '') : url
   end
 end
